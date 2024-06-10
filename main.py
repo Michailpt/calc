a322dcc4b35907de9a3c821f94b3a541d9a57402
Michailpt <kraimonn@gmail.com>
2024-06-10 11:49:36 +0300

    6

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ today = dt.datetime.today().date()
 date_format = '%d.%m.%Y'
 
 class CaloriesCalculator:
-    """Калькулятор калорий7"""
+    """Калькулятор калорий8"""
     def __init__(self, limit):
         self.limit = limit
         self.records = []
